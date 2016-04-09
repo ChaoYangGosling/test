42b19149bd41b3c5ede1137d4dc09810022abd6c
lql-1 <530807263@qq.com>
2016-04-09 22:27:49 +0800

    测试

---
+++ src/test1/Zcy.java
@@ -0,0 +1,25 @@
+package test1;
+
+public class Zcy {
+	private String name;
+	private int age;
+	private int wife;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getWife() {
+		return wife;
+	}
+	public void setWife(int wife) {
+		this.wife = wife;
+	}
+}
